e0da3e6a72ea3ba28f902c367154779dbb982ed1
Grant Murphy <grantsmurphy@gmail.com>
2019-01-22 11:00:47 -0500

    adding Spring Config. Closes #22

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/Services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/Services/map/OwnerServiceMap.java
@@ -1,11 +1,12 @@
 package guru.springframework.sfgpetclinic.Services.map;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
 import guru.springframework.sfgpetclinic.Services.OwnerService;
 import guru.springframework.sfgpetclinic.model.Owner;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
